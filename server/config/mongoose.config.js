ed2d899b744813ccc7da3f710f38982ab5c9ff5c
shu-william <williamshu@outlook.com>
2023-06-11 01:37:58 -0700

    created base project structure

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const dbName = "movieDB";
+mongoose.connect(`mongodb://localhost/${dbName}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(()=>console.log(`Connected to ${dbName} database!`))
+    .catch((err)=>console.log(err));      
