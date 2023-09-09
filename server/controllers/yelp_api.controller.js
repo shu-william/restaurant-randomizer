960a1a89bb93cf2074590405d009788956b770bf
shu-william <williamshu@outlook.com>
2023-09-09 01:14:46 -0700

    comments

--- server/controllers/yelp_api.controller.js
+++ server/controllers/yelp_api.controller.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 
 const API_KEY = process.env.API_KEY;
 
+// NOTE: The current version of this app does not filter by rating, since the Yelp API does not take that as a query in the initial search.
+// If trying to sort by rating, you would need to run a loop to gather all data for one search query using offset, then filter out ratings manually.
+// Since this requires a higher volume of searches, I have not implemented this feature at this time due to the current limit of 500 API calls per day.
 module.exports.getRestaurantsByLocation = (req, res) => {
     let price = req.query.cost.join();
     let categories = req.query.cuisine.join();
@@ -29,7 +32,7 @@ module.exports.getRestaurantsByLocation = (req, res) => {
             )
             res.send(data);
         } catch (err) { 
-            console.error(err) 
+            console.log(err);
         };    
     }
 
